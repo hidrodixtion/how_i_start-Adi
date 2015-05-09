a5e1ecf97edb3715199a082d0f70961830e1dde1
Adi Nugroho <hidrodixtion@gmail.com>
2015-05-09 20:23:03 +0700

    Initial commit

---
+++ lib/how_i_start/Adi.rb
@@ -0,0 +1,7 @@
+require "how_i_start/Adi/version"
+
+module HowIStart
+  module Adi
+    # Your code goes here...
+  end
+end
---
+++ lib/how_i_start/Adi/version.rb
@@ -0,0 +1,5 @@
+module HowIStart
+  module Adi
+    VERSION = "0.0.1"
+  end
+end
